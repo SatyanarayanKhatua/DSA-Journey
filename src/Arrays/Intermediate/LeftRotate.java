42070e697698f77a6be374266733a832f84b6778
Satya <satyaknk2000@gmail.com>
2026-07-10 00:01:13 +0530

    Soveld Intermediate Array Problem Rotate Array by K positions

---
+++ src/Arrays/Intermediate/LeftRotate.java
@@ -0,0 +1,41 @@
+package Arrays.Intermediate;
+
+public class LeftRotate {
+
+    public static void leftRotate(int[] arr, int k) {
+
+        int n = arr.length;
+
+        k = k % n;
+
+        for (int i = 0; i < k; i++) {
+
+            int first = arr[0];
+
+            for (int j = 0; j < n - 1; j++) {
+
+                arr[j] = arr[j + 1];
+
+            }
+
+            arr[n - 1] = first;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,7};
+
+        leftRotate(arr,2);
+
+        for(int num : arr){
+
+            System.out.print(num+" ");
+
+        }
+
+    }
+
+}
