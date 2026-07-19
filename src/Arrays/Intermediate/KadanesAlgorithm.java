e5f70a62451427b84836f5d8fe24c3bdc89c06d0
Satya <satyaknk2000@gmail.com>
2026-07-19 23:42:32 +0530

    Kadane's Algorithm (Maximum Subarray Sum)

---
+++ src/Arrays/Intermediate/KadanesAlgorithm.java
@@ -0,0 +1,35 @@
+package Arrays.Intermediate;
+
+public class KadanesAlgorithm {
+
+    public static int maxSubArray(int[] arr) {
+
+        int currentSum = 0;
+        int maxSum = arr[0];
+
+        for (int num : arr) {
+
+            currentSum += num;
+
+            maxSum = Math.max(maxSum, currentSum);
+
+            if (currentSum < 0) {
+
+                currentSum = 0;
+
+            }
+
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+
+        System.out.println(maxSubArray(arr));
+
+    }
+
+}
