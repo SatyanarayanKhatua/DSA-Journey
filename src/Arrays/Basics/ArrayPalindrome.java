4c4e58dc4c8525a4389beee98850cd8d4e064812
Satya <satyaknk2000@gmail.com>
2026-07-20 23:28:30 +0530

    Check Whether an Array is a Palindrome

---
+++ src/Arrays/Basics/ArrayPalindrome.java
@@ -0,0 +1,35 @@
+package Arrays.Basics;
+
+public class ArrayPalindrome {
+
+    public static boolean isPalindrome(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            if (arr[left] != arr[right]) {
+
+                return false;
+
+            }
+
+            left++;
+            right--;
+
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,2,1};
+
+        System.out.println(isPalindrome(arr));
+
+    }
+
+}
