dd4efc7307e620e53189702827abf64bd143ae63
Satya <satyaknk2000@gmail.com>
2026-07-22 23:43:56 +0530

    Count Vowels and Consonants String Program

---
+++ src/Strings/CountVowelsConsonants.java
@@ -0,0 +1,44 @@
+package Strings;
+
+public class CountVowelsConsonants {
+
+    public static void count(String str) {
+
+        int vowels = 0;
+        int consonants = 0;
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                if (ch == 'a' || ch == 'e' ||
+                        ch == 'i' || ch == 'o' ||
+                        ch == 'u') {
+
+                    vowels++;
+
+                } else {
+
+                    consonants++;
+
+                }
+
+            }
+
+        }
+
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+    }
+
+    public static void main(String[] args) {
+
+        count("Hello World");
+
+    }
+
+}
