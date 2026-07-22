a8ba0468b77c59217762e55282530036e2719d53
Satya <satyaknk2000@gmail.com>
2026-07-22 23:40:06 +0530

    Reverse String Problem

---
+++ src/Strings/Basics/ReverseString.java
@@ -0,0 +1,31 @@
+package Strings.Basics;
+
+public class ReverseString {
+
+    public static String reverse(String str) {
+
+        char[] arr = str.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        System.out.println(reverse(str));
+    }
+}
